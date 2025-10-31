ec2b54a40ea74bfdf0a76e57d309160286ac4eef
Faraz5658 <farazahmad5623@gmail.com>
2025-10-31 18:40:40 +0500

    ternary operator

---
+++ Operators/Ternary Operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1=30,num2=18,max;
+	cout<<"ENTER THE 1st NUMBER : "<<num1<<endl;
+	cout<<"ENTER THE 2nd NUMBER : "<<num2<<endl;
+	max = (num1>num2)?  num1 : num2;
+	cout<<"THE MAX NUMBER IS : "<<max<<endl;
+	return 0;
+}
